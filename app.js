2f9c2d0b9f6af0d3fcc5f45e2d1d57b0fb0738ba
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-04 22:33:58 -0600

    PUT a specific article

--- app.js
+++ app.js
@@ -73,6 +73,19 @@ app.route("/articles/:articleTitle")
     })
 })
 
+.put(function(req,res) {
+    Article.updateOne(
+        {title:req.params.articleTitle},
+        {title:req.body.title,content:req.body.content},
+        {overwrite:true},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article")
+            }
+        }
+    )
+})
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 })
